f2956a1099581ffdb9637ffe0ed460a7bd24ef5f
Alison Gustavo Pena da Silva <106981094+alisongpds@users.noreply.github.com>
2023-06-21 12:48:47 -0300

    Teste Cadastro_de_Produto funcionando

--- tests/test_Cadastro_de_Produtos.py
+++ tests/test_Cadastro_de_Produtos.py
@@ -1,8 +1,8 @@
-from Produto import Produto
 from Cadastro_de_Produto import Cadastro_de_Produto
+from Produto import Produto
 
 def test_para_cadastro_de_produtos():
-    produto = Produto()
-    cadastro_produto = cadastroProduto()
+    produto = Produto ("nome", 100, "tipo", "departamento", 50)
+    cadastro_produto = Cadastro_de_Produto()
     resposta = cadastro_produto.cadastrar_produto(produto)
     assert "Produto cadastrado com sucesso!" == resposta
